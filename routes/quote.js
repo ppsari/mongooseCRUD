0e0c617289479f0d70de415bc154b17e423d64a5
ppsari <holie.nait@gmail.com>
2018-02-26 16:02:34 +0700

    routes_get

--- routes/quote.js
+++ routes/quote.js
@@ -1,9 +1,20 @@
 const express = require('express')
 let router = express.Router()
 
+let Quote = require('../models/quote')
+
 router.get('/', function(req, res) {
-  res.status(200)
-  res.send('alive')
+
+  Quote.find({}, function(err, results){
+    if (err) {
+      res.status(500)
+      res.send({err:err})
+    } else {
+      res.status(200)
+      res.send(results)
+    }
+  })
+
 })
 
 router.post('/', function(req, res) {
