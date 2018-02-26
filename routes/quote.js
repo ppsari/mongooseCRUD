1fb4da76b0f3487c91cb4a7ee34cef8de118af22
ppsari <holie.nait@gmail.com>
2018-02-26 16:09:03 +0700

    getOne, put, delete

--- routes/quote.js
+++ routes/quote.js
@@ -3,6 +3,7 @@ let router = express.Router()
 
 let Quote = require('../models/quote')
 
+
 router.get('/', function(req, res) {
 
   Quote.find({}, function(err, results){
@@ -17,10 +18,73 @@ router.get('/', function(req, res) {
 
 })
 
-router.post('/', function(req, res) {
-  res.status(200)
-  res.send('alive in posts')
+router.get('/:id', function(req, res) {
+  let id = req.params.id
+
+  Quote.findById(id, function(err, quote) {
+    if (err) {
+      res.status(500)
+      res.send({err:err})
+    } else {
+      res.status(200)
+      res.send(quote)
+    }
+  })
 })
 
+router.post('/', function(req, res) {
+  let quote = new Quote(req.body)
+  quote.save(function(err, save_quote) {
+    if (err) {
+      res.status(500)
+      res.send({err:err})
+    } else {
+      res.status(200)
+      res.send(save_quote)
+    }
+  })
+})
+
+router.put('/:id', function(req, res) {
+  let id = req.params.id
+
+  Quote.findById(id, function(err, quote) {
+    if (err) {
+      res.status(500)
+      res.send({err:err})
+    } else {
+      quote.save(function(err, updt_quote) {
+        if (err) {
+          res.status(500)
+          res.send({err:err})
+        } else {
+          res.status(200)
+          res.send(updt_quote)
+        }
+      })
+    }
+  })
+})
+
+router.delete('/:id', function(req, res) {
+  let id = req.params.id
+
+  Quote.findById(id, function(err, quote) {
+    if (err) {
+      res.status(500)
+      res.send({err:err})
+    } else {
+      quote.remove(function(err, dlt_quote) {
+        if (err) {
+          res.status(500)
+          res.send({err:err})
+        } else {
+          res.status(200)
+          res.send(dlt_quote)
+        }
+      })
+    }
+  })
+})
 
 module.exports = router
